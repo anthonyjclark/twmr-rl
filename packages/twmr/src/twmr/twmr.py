95783531a420737a58b16370038d06f621291fc2
anthonyjclark <anthonyjclark@gmail.com>
2025-12-18 16:44:54 -0800

    Working on demo.

--- packages/twmr/src/twmr/twmr.py
+++ packages/twmr/src/twmr/twmr.py
@@ -1,23 +1,56 @@
+from pathlib import Path
+
 from jax import Array as JaxArray
-from ml_collections.config_dict import ConfigDict
-from mujoco import MjModel
+from ml_collections import config_dict
+from mujoco import MjModel, mjx  # type: ignore
 from mujoco.mjx import Model as MjxModel
 from mujoco_playground import MjxEnv, State, dm_control_suite
+from mujoco_playground._src.dm_control_suite import common
 
-# def default_vision_config() -> ConfigDict: ...
+ConfigOverridesDict = dict[str, str | int | list]
+_XML_PATH = Path(__file__).parent / "assets" / "wheeled_mobile_robot.xml"
 
 
-def default_config() -> ConfigDict: ...
+def default_vision_config() -> config_dict.ConfigDict:
+    return config_dict.create(
+        gpu_id=0,
+        render_batch_size=512,
+        render_width=64,
+        render_height=64,
+        enable_geom_groups=[0, 1, 2],
+        use_rasterizer=False,
+        history=3,
+    )
+
+
+# TODO: check all of these default values
+def default_config() -> config_dict.ConfigDict:
+    return config_dict.create(
+        ctrl_dt=0.01,
+        sim_dt=0.01,
+        episode_length=1000,
+        action_repeat=1,
+        vision=False,
+        vision_config=default_vision_config(),
+        impl="warp",  # TODO: cartpole uses jax
+        nconmax=0,
+        njmax=2,
+    )
 
 
 class TransformableWheelMobileRobot(MjxEnv):
     def __init__(
         self,
         # Task specific config
-        config: ConfigDict = default_config(),
-        config_overrides: dict[str, str | int | list] | None = None,
+        config: config_dict.ConfigDict = default_config(),
+        config_overrides: ConfigOverridesDict | None = None,
     ):
         super().__init__(config, config_overrides)
+
+        self._xml_path = _XML_PATH.as_posix()
+        self._model_assets = common.get_assets()
+        self._mj_model = MjModel.from_xml_string(_XML_PATH.read_text(), self._model_assets)
+        self._mjx_model = mjx.put_model()
 
     def reset(self, rng: JaxArray) -> State: ...
 
